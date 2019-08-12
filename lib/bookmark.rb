ff7913522f8d03c3cc4615edc0113be0e6fae3a5
Mario Garcia Pinan <jaitoone@gmail.com>
2019-08-12 17:40:38 +0100

    Add class method to interact with postgres database

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,30 @@
+require 'pg'
+
 class BookmarkModel 
   def self.all
-    [
-      'abc',
-      'def',
-      'efg'
-    ]
-  end
+      
+      con = PG.connect :dbname => 'bookmark_manager', :user => 'student'
+      
+      rs = con.exec "SELECT * FROM bookmarks LIMIT 3"
 
+      @txt = ""
+
+      rs.each do |row|
+        @txt +=  '%s %s' % [ row['id'], row['url']]
+      end
+
+    #rescue PG::Error => e
+
+      #puts e.message
+      
+
+    #ensure 
+
+      con.close if con
+      @txt
+      
+
+    #end
+
+  end
 end

6292ccb72d562cd93f778557fbe528f3d755e773
Mario Garcia Pinan <jaitoone@gmail.com>
2019-08-15 14:45:55 +0100

    Step 10 finished

--- web_helpers.rb
+++ web_helpers.rb
@@ -1,13 +1,13 @@
 def insert
 
   #Connects our model with the Database
-  connection = PG.connect(dbname: 'bookmark_manager_test')
+  
 
   # Add the test data
-  connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-  connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.askjeeves.com');")
-  connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
-  connection.exec("INSERT INTO bookmarks VALUES(4, 'http://www.twitter.com');")
+  BookmarkModel.create(url: 'http://www.makersacademy.com')
+  BookmarkModel.create(url:'http://www.askjeeves.com')
+  BookmarkModel.create(url: 'http://www.google.com')
+  BookmarkModel.create(url: 'http://www.twitter.com')
 
 
 end
